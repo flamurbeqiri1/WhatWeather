7c57b32ea7daa02b49953d5777f2461d17bcbd0b
Flamur Beqiri <flamurbeqiri1@gmail.com>
2019-06-27 22:37:32 +0200

    refactor data source in extension

--- WhatWeather/ViewControllers/Main/LocationTableViewController.swift
+++ WhatWeather/ViewControllers/Main/LocationTableViewController.swift
@@ -26,7 +26,21 @@ class MainTableViewController: UITableViewController {
 
     }
 
-    // MARK: - Table view data source
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+// MARK: - Table view data source
+
+extension MainTableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         return LocationSection.numberOfSections
@@ -61,15 +75,4 @@ class MainTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         print(indexPath.row)
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
